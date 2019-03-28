f5b0d37be1058a22dc173cbe21b235183a71eb9b
huyuangang <huyuangang@didichuxing.com>
2019-03-28 11:25:04 +0800

    修改导航栏与目录对应方式

--- src/js/custom/navbar.js
+++ src/js/custom/navbar.js
@@ -3,29 +3,20 @@ var $ = require('jquery');
 
 var gitbook = window.gitbook;
 
-gitbook.events.on('page.change', init);
+
+// 页面更改后更新目录
+gitbook.events.on('page.change', change);
 
 
-function init(){
-  var currentPart = window.localStorage.getItem('custom_current_part');
-  var customConfig = window.gitbook.state.config.pluginsConfig['theme-custom'];
-  // 第一次进来的时候
-  if (!currentPart || currentPart === "undefined") {
-    if (customConfig && customConfig.navbar && customConfig.navbar.nav) {
-      var defaultIndex = customConfig.navbar.nav.findIndex(function(item){ return !!item.default });
-      currentPart = customConfig.navbar.nav[defaultIndex > -1 ? defaultIndex : 0].part;
-      window.localStorage.setItem('custom_current_part', currentPart);
-    }
-  }
-
-  // 绑定点击事件
-  $('.book-nav-list-link').click(function() {
-    window.localStorage.setItem('custom_current_part', $(this).attr('data-part'));
-  })
-
+function change(){
+  var part = parseInt(gitbook.state.level, 10);
+  var currentPart = gitbook.state.config.pluginsConfig['theme-custom'].navbar.nav[part - 1].part;
   // 设置当前part nav激活状态
   $('.book-nav-list-link').each(function() {
     var _this = $(this);
+    if (_this.hasClass('book-nav-list-link-active')) {
+      _this.removeClass('book-nav-list-link-active');
+    }
     if (_this.attr('data-part') === currentPart) {
       _this.addClass('book-nav-list-link-active');
     }
